7068c0887bddc40b694b03532b18c476b39be3e5
code1031 <779219088@qq.com>
2017-09-03 17:51:40 +0800

    添加表单验证插件

--- public/js/teacher-add.js
+++ public/js/teacher-add.js
@@ -1,4 +1,4 @@
-define(['jquery','template','util','datepicker','language'], function ($,template,util) {
+define(['jquery','template','util','datepicker','language','validate'], function ($,template,util) {
     util.setMenu('/teacher/list');
     //获取编辑讲师 的id
     var tcId=util.qs('tc_id');
@@ -26,25 +26,47 @@ define(['jquery','template','util','datepicker','language'], function ($,templat
         submitForm('/api/teacher/add');
     }
     //根据id查询对应的讲师详细信息
-    function submitForm(url) {
-        $('#formBtn').click(function () {
-
-
-            $.ajax({
-                type: 'post',
-                url: url,
-                data: $('#formId').serialize(),
-                dataType: 'json',
-                success: function (data) {
-                    console.log(data);
-                    if (data.code == 200) {
-                        location.href = '/teacher/list';
-                    }
+    function submitForm(url){
+        $('#formId').validate({
+            sendForm:false,
+            valid: function () {
+                console.log(123);
+            },
+            description:{
+                tc_name:{
+                    required:'用户名不能为空',
+                    valid:'√'
+                },
+                tc_pass:{
+                    required:'密码不能为空',
+                    pattern:'必须是6位数字',
+                    valid:'√'
+                },
+                tc_join_date:{
+                    required:'出生日期不能为空',
+                    valid:'√'
                 }
-
-            })
-
+            }
         })
+    //function submitForm(url) {
+    //    $('#formBtn').click(function () {
+    //
+    //
+    //        $.ajax({
+    //            type: 'post',
+    //            url: url,
+    //            data: $('#formId').serialize(),
+    //            dataType: 'json',
+    //            success: function (data) {
+    //                console.log(data);
+    //                if (data.code == 200) {
+    //                    location.href = '/teacher/list';
+    //                }
+    //            }
+    //
+    //        })
+    //
+    //    })
     }
     })
 
